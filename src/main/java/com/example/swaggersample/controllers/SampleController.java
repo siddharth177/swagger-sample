5421b6b86022c267df2b872ef38862174772bde5
= <=>
2024-08-18 21:00:31 +0530

    provided open API definition

--- src/main/java/com/example/swaggersample/controllers/SampleController.java
+++ src/main/java/com/example/swaggersample/controllers/SampleController.java
@@ -1,6 +1,9 @@
 package com.example.swaggersample.controllers;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Swagger Sample Application",
+                description = "Sample Application to demonstrate use and properties of swagger.",
+                version = "1.0.0",
+                contact = @Contact(
+                        name = "Siddharth Katiyar",
+                        email = "siddharth.dev177@gmail.com"
+
+                )
+        )
+)
 @Tag(name = "SampleController", description = "This controller is created to provide details of all swagger properties")
 public class SampleController {
 
