fc4421aebd210f2c4fc4dc3a9a9db5447a5bb755
= <=>
2024-08-23 15:17:12 +0530

    optimization

--- src/main/java/com/example/swaggersample/models/Student.java
+++ src/main/java/com/example/swaggersample/models/Student.java
@@ -1,15 +1,11 @@
 package com.example.swaggersample.models;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.media.SchemaProperty;
 import lombok.Data;
 import lombok.NonNull;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import static com.example.swaggersample.services.StudentService.students;
 
 @Data
 @Schema(    // used on a POJO
